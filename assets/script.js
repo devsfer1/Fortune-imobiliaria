c4cfc9e91578ed907169836132150e57d18235f2
Fernando Chaves <fernando.chaves16@hotmail.com>
2020-12-28 18:09:50 -0300

    Logo nav atualizado

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,8 @@ const animate =  'animate';
 //Variaveis Header
 var menuBtn = document.querySelector('.nav__bars'),
     burger = document.querySelector('.nav__burger'),
-    navLogo = document.querySelector('.nav__logo'),
+    navImg1 = document.getElementById('nav-img-1');
+    navImg2 = document.getElementById('nav-img-2');
     navList = document.querySelector('.nav__list'),
     navbar = document.querySelector('.nav'),
     header = document.querySelector('#header'),
@@ -28,7 +29,8 @@ function stickyNav() {
     header.classList.toggle('sticky', window.scrollY > 20);
     //Mudando cores items nav
     if(window.scrollY > 20) {
-        navLogo.style.color = '#599e8e';
+        navImg2.classList.remove('nav__img--hidden');
+        navImg1.classList.add('nav__img--hidden');
         burger.classList.add('burger--sticky');
         burger.classList.add('nav__burger--sticky');
         for(var i = 0; i < navLink.length; i++) {
@@ -36,7 +38,8 @@ function stickyNav() {
             navbar.style.padding = '25px 0';
         }
     } else {
-        navLogo.style.color = '';
+        navImg1.classList.remove('nav__img--hidden');
+        navImg2.classList.add('nav__img--hidden');
         navbar.style.padding = '';
         burger.classList.remove('nav__burger--sticky');
         burger.classList.remove('burger--sticky');
